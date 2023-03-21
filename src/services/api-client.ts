f9044c8faf204d739315302f80cb5206a9db55f1
Phinna Huang <phinna37@gmail.com>
2023-03-21 12:25:25 -0700

    Remove API key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '1febad23f92541aba9f52c7ba742ac35'
+        key: process.env.RAWG_API_KEY
     }
 })

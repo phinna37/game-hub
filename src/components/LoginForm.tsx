42113f79e211d30e97ccfa29015f4ba9dac1a929
Phinna Huang <phinna37@gmail.com>
2023-03-21 11:18:38 -0700

    Add validation to the login form

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,17 +5,43 @@ import {
   Container,
   Flex,
   FormControl,
+  FormHelperText,
   FormLabel,
   GridItem,
   Heading,
   Input,
 } from "@chakra-ui/react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z
+    .string()
+    .refine(
+      (value) => /(?=(.*[a-z]){3,})/.test(value),
+      "at least 3 lowercase letters"
+    )
+    .refine(
+      (value) => /(?=.*[!@#$&*])/.test(value),
+      "at least 1 special case letter"
+    )
+    .refine((value) => /(?=.*[0-9])/.test(value), "at least one digits")
+    .refine((value) => /(?=.*[A-Z])/.test(value), "at least one uppercase"),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const LoginForm = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Submitted");
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
     <GridItem area="main">
       <Container centerContent>
@@ -37,14 +63,20 @@ const LoginForm = () => {
               <Heading>Login</Heading>
             </Box>
             <Box my={4} textAlign="left">
-              <form onSubmit={(e) => handleSubmit(e)}>
+              <form onSubmit={handleSubmit(onSubmit)}>
                 <FormControl isRequired>
                   <FormLabel>Email</FormLabel>
-                  <Input placeholder="test@test.com" />
+                  <Input placeholder="test@test.com" {...register("email")} />
+                  {errors.email && (
+                    <FormHelperText>{errors.email.message}</FormHelperText>
+                  )}
                 </FormControl>
                 <FormControl mt={6} isRequired>
                   <FormLabel>Password</FormLabel>
-                  <Input placeholder="*****" />
+                  <Input placeholder="*****" {...register("password")} />
+                  {errors.password && (
+                    <FormHelperText>{errors.password.message}</FormHelperText>
+                  )}
                 </FormControl>
                 <Button
                   width="full"
